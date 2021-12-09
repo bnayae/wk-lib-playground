49a1275f9716f0c574547204a80f4076e77d114d
Bnaya Eshet <3382669+bnayae@users.noreply.github.com>
2021-12-09 13:36:13 +0200

    [ADD] styles component

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,22 @@
+import { Layout } from './Layout';
+import {pallette} from './pallette'
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './GlobalStyle';
+
+const withThemeProvider = (Story, context) => {  
+  return (
+      <ThemeProvider theme={{ pallette }}>
+        <GlobalStyle />
+        <Layout>
+          <Story {...context} />
+        </Layout>
+      </ThemeProvider>
+  );
+};
+
+// addDecorator(withInfo());
+export const decorators = [withThemeProvider];
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
